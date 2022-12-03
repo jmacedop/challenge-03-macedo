898302c1c0b3bf6573255a1533d646f774f709b5
Jimmy Ronaldo Macedo Pizango <jmacedop@emeal.nttdata.com>
2022-12-03 16:20:09 -0500

    first commit

---
+++ challenge-03/DetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsViewController.swift
+//  challenge-03
+//
+//  Created by Jimmy Ronaldo Macedo Pizango on 2/12/22.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ challenge-03/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  challenge-03
+//
+//  Created by Jimmy Ronaldo Macedo Pizango on 2/12/22.
+//
+
+import Foundation
